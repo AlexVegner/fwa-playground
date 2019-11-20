237f472800454fc586231c0d6d62c56990da86a1
Alex Vegner <alexvegner@gmail.com>
2019-11-21 00:53:40 +0200

    weak 7

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
           textTheme: TextTheme(
               body1: TextStyle(
@@ -32,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,113 +29,83 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _selectedButton;
-
-  @override
-  void initState() {
-    _selectedButton = 'init state';
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      appBar: AppBar(
+        title: Text('First screen'),
+      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 100,
-              color: Colors.red,
-              child: Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.camera),
-                    onPressed: () {
-                      if (mounted) {
-                        setState(() {
-                          _selectedButton = 'camera';
-                        });
-                      }
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      setState(() {
-                        _selectedButton = 'add';
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.remove),
-                    onPressed: () {
-                      setState(() {
-                        _selectedButton = 'remove';
-                      });
-                    },
-                  ),
-                ],
+        child: Container(
+          decoration: new BoxDecoration(
+            color: Colors.purple,
+            gradient: new LinearGradient(
+              colors: [Colors.red, Colors.cyan],
+            ),
+          ),
+          height: 500,
+          width: 300,
+          // color: Colors.blue,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text('FlatButton'),
+                      onPressed: () {},
+                    ),
+                    RaisedButton(
+                      child: Text('RaisedButton'),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {},
+                    ),
+                    Icon(Icons.add, size: 60, color: Colors.blue,),
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+                      child: Text('sdfsdf'),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        text: 'Hello', // default text style
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: ' beautiful ',
+                              style: TextStyle(fontStyle: FontStyle.italic)),
+                          TextSpan(
+                              text: 'world',
+                              style: TextStyle(fontWeight: FontWeight.w100)),
+                              
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            Container(
-              height: 100,
-              color: Colors.green,
-              child: Text(_selectedButton,
-                  style: Theme.of(context).textTheme.body1.copyWith(
-                        fontSize: 40,
-                      )),
-            ),
-            Container(
-              height: 100,
-              color: Colors.blue,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      color: Colors.red,
-                      child: new Text("Babe")),
-                    flex: 1,
-                  ),
-                  Expanded(
-                    child: new Text("I miss you"),
-                    flex: 1,
-                  )
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
-      // / This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {}),
+        tooltip: 'Increment Counter',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
